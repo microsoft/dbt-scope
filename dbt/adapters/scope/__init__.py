1f208549c4c3fd274a1711c2284aafad97159f66
Raki Rahman <mdrakiburrahman@gmail.com>
2026-04-06 00:30:27 +0000

    Fix packaging bug

--- dbt/adapters/scope/__init__.py
+++ dbt/adapters/scope/__init__.py
@@ -5,9 +5,10 @@ from dbt.adapters.scope.connections import ScopeConnectionManager  # noqa: F401
 from dbt.adapters.scope.credentials import ScopeCredentials
 from dbt.adapters.scope.impl import ScopeAdapter
 from dbt.adapters.scope.relation import ScopeRelation  # noqa: F401
+from dbt.include import scope as include_scope
 
 Plugin = AdapterPlugin(
     adapter=ScopeAdapter,
     credentials=ScopeCredentials,
-    include_path="dbt/include/scope",
+    include_path=include_scope.PACKAGE_PATH,
 )
